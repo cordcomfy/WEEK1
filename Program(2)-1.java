f8c299155ae4fca1ff6430584177b52020645781
cordcomfy <bachinakeerthi2@gmail.com>
2025-04-20 22:36:13 +0530

    Add files via upload

---
+++ Program(2)-1.java
@@ -0,0 +1,10 @@
+class Main
+{
+     public static void main(String[] args)
+
+     {
+        System.out.println("Name: B.KEERTHI");
+         System.out.println("Section: CSE-A");
+         System.out.println("Roll No: 24018");
+      }
+}
